a33b5ddcfd89a46cafc4e3735ef278c23bcb4602
Philip Bradley <pdbradley@gmail.com>
2015-05-02 16:32:03 -0400

    initial

---
+++ vim_formatter.rb
@@ -0,0 +1,19 @@
+# this formatter is referenced from my rspec shortcuts in vimrc.  thanks to ronin on rails
+class VimFormatter
+  RSpec::Core::Formatters.register self, :example_failed
+
+  def initialize(output)
+    @output = output
+  end
+
+  def example_failed(notification)
+    @output << format(notification) + "\n"
+  end
+
+  private
+
+  def format(notification)
+    rtn = "%s: %s" % [notification.example.location, notification.exception.message]
+    rtn.gsub("\n", ' ')[0,160]
+  end
+end
